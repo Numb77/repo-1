f5254d257fc319c68daa4379ef51159398b95ec6
Numb77 <amitmiron77@gmail.com>
2021-09-14 04:50:12 -0700

    client side hook

--- script.sh
+++ script.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-echo "Main branch rules!"
-$branch git branch | grep main
+branch=$(git rev-parse --abbrev-ref HEAD)
+if ["$branch" = "master"]; then
+	echo "Notifying release to everyone ..."
+	# send notification ...
+fi
